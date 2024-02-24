5799cfe306cd7bac0684e6fcd072a2dce1ffb944
Heumayr <80398233+Heumayr@users.noreply.github.com>
2024-02-24 21:10:58 +0100

    Add project files.

---
+++ Generic/MyList.cs
@@ -0,0 +1,68 @@
+﻿using Inheritance_Area;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class MyList<T>
+        where T : Forms
+    {
+        public int Count { get; private set; }
+
+        private class Data
+        {
+            public Data(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+            public Data? Next { get; set; }
+        }
+
+        private Data? Head { get; set; }
+        private Data? Last { get; set; }
+
+        public void Add(T data)
+        {
+            if (Head == null)
+            {
+                Head = new Data(data);
+                Last = Head;
+            }
+            else
+            {
+                Head.Next = new Data(data);
+
+                Head = Head.Next;
+            }
+            Console.WriteLine("Added: " + data.Area);
+            Count++;
+        }
+
+        public T? ElementAt(int index)
+        {
+            if (Head == null)
+                return default;
+
+            if (index < 0 || index >= Count)
+                return default;
+
+            if (index == Count - 1)
+                return Head.Value;
+
+            var result = Last;
+
+            for (int i = 0; i < index; i++)
+            {
+                if (result != null)
+                    result = result.Next;
+            }
+
+            return result != null ? result.Value : default;
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,42 @@
+﻿using Inheritance_Area;
+
+namespace Generic
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //    var myList = new MyList<int>();
+
+            //    myList.Add(5);
+            //    myList.Add(10);
+            //    myList.Add(15);
+            //    myList.Add(20);
+
+            //    Console.WriteLine(myList.ElementAt(0));
+
+            //    var myList2 = new MyList<string>();
+
+            //    myList2.Add("Richard");
+            //    myList2.Add("Franz");
+            //    myList2.Add("Andi");
+
+            var mylist3 = new MyList<Forms>();
+
+            var circle = new Circle(4, "Circle A");
+
+            var rectangle = new Rectangle(10, 5, "Rect A");
+
+            var square = new Square(3, "Square A")
+            {
+                Name2 = "Name 2"
+            };
+
+            mylist3.Add(circle);
+            mylist3.Add(rectangle);
+            mylist3.Add(square);
+
+            Console.WriteLine(mylist3.ElementAt(1));
+        }
+    }
+}
